18a56bc66d9e73f2cd9fcf09586500fe1a8995c1
LTH <johtta888@naver.com>
2023-03-24 20:29:49 +0900

    Feat: 추가 typescript eslint, base url src

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,20 @@ module.exports = {
     '@react-native-community',
     'airbnb',
     'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:prettier/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
 };
