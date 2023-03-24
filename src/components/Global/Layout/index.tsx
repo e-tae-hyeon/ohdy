1c8acd18a042a7b0ada919c024a147e1621d31c1
LTH <johtta888@naver.com>
2023-03-25 01:48:00 +0900

    Feat: 추가 header prop to Layout

--- src/components/Global/Layout/index.tsx
+++ src/components/Global/Layout/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import {View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 type LayoutProps = {
+  header?: React.ReactNode;
+  className?: string;
   children: React.ReactNode;
 };
 
-function Layout({children}: LayoutProps) {
+function Layout({header, className, children}: LayoutProps) {
   return (
-    <SafeAreaView className="flex flex-1 bg-white">{children}</SafeAreaView>
+    <SafeAreaView className="flex flex-1 bg-white">
+      {header}
+      <View className={`flex-1 p-4 ${className}`}>{children}</View>
+    </SafeAreaView>
   );
 }
 
