46ccd6eabf0657e3bbe2314f41a135834fafe1df
LTH <johtta888@naver.com>
2023-03-26 21:20:35 +0900

    Design: 수정 BottomSheet style

--- src/components/@base/BottomSheet/index.tsx
+++ src/components/@base/BottomSheet/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import {View} from 'react-native';
 import Modal from 'react-native-modal';
 
 type BottomSheetProps = {
   isVisible: boolean;
+  onBackDrop?: () => void;
   children: React.ReactNode;
 };
 
-function BottomSheet({isVisible, children}: BottomSheetProps) {
+function BottomSheet({isVisible, onBackDrop, children}: BottomSheetProps) {
   return (
-    <Modal isVisible={isVisible} className="justify-end m-0 bg-white">
-      {children}
+    <Modal
+      isVisible={isVisible}
+      onBackButtonPress={onBackDrop}
+      onBackdropPress={onBackDrop}
+      className="justify-end m-0">
+      <View className="px-4 py-8 bg-white rounded-t-2xl">{children}</View>
     </Modal>
   );
 }
