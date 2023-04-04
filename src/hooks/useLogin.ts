047ba2c00cedd94579f97fcda52cec3a5f496797
LTH <johtta888@naver.com>
2023-04-04 18:12:02 +0900

    Feat: 추가 로그인 시, create profile 유무 확인

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -4,14 +4,17 @@ import tokenStorage from 'storages/tokenStorage';
 import useUserStore from 'stores/useUserStore';
 import {getErrorMessage} from 'utils/error';
 import useToast from './useToast';
+import {useQueryClient} from '@tanstack/react-query';
 
 function useLogin() {
+  const queryClient = useQueryClient();
   const {setUser} = useUserStore();
   const {showToast} = useToast();
 
   return async ({user, tokens}: LoginUserData) => {
     try {
       applyTokenClient(tokens.accessToken);
+      await queryClient.invalidateQueries(['myProfile']);
       setUser(user);
       await tokenStorage.set(tokens);
     } catch (err) {
