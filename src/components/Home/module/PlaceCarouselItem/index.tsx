ced8999f0aefcb00b3d456da81eb68b5ba2623ba
LTH <johtta888@naver.com>
2023-04-18 18:16:18 +0900

    Fix: 수정 PlaceCarousel Current Item 일 때 pressable
    PlaceCarouselItem max height 360 추가

--- src/components/Home/module/PlaceCarouselItem/index.tsx
+++ src/components/Home/module/PlaceCarouselItem/index.tsx
@@ -3,39 +3,52 @@ import colors from 'common/styles/colors';
 import AppText from 'components/@base/AppText';
 import FlexView from 'components/@base/FlexView';
 import React from 'react';
-import {ImageBackground} from 'react-native';
+import {ImageBackground, Pressable} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 type PlaceCarouselItemProps = {
   place: Place;
+  onPress: (id: number) => void;
 };
 
-function PlaceCarouselItem({place}: PlaceCarouselItemProps) {
+function PlaceCarouselItem({place, onPress}: PlaceCarouselItemProps) {
   const {id, name, description, coverImages, category, location} = place;
 
   return (
-    <ImageBackground
-      source={{uri: coverImages[0].uri}}
-      className="w-[70vw] h-[55vh] justify-end">
-      <LinearGradient
-        colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,0.5)', 'rgba(0,0,0,1)']}
-        className="px-4 py-8">
-        <FlexView>
-          <FlexView direction="row" gapSize="small" className="items-baseline">
-            <AppText typoStyle="H3" color={colors.white}>
-              {name}
-            </AppText>
+    <Pressable onPress={() => onPress(id)}>
+      <ImageBackground
+        source={{uri: coverImages[0].uri}}
+        className="w-[70vw] h-[55vh] justify-end"
+        style={{maxHeight: 360}}
+        imageStyle={{maxHeight: 360}}>
+        <LinearGradient
+          colors={[
+            'rgba(0,0,0,0.1)',
+            'rgba(0,0,0,0.25)',
+            'rgba(0,0,0,0.3)',
+            'rgba(0,0,0,0.6)',
+          ]}
+          className="px-4 py-8">
+          <FlexView>
+            <FlexView
+              direction="row"
+              gapSize="small"
+              className="items-baseline">
+              <AppText typoStyle="H3" color={colors.white}>
+                {name}
+              </AppText>
+              <AppText typoStyle="B2" color={colors.white}>
+                {category.name}
+              </AppText>
+            </FlexView>
             <AppText typoStyle="B2" color={colors.white}>
-              {category.name}
+              {description}
             </AppText>
           </FlexView>
-          <AppText typoStyle="B2" color={colors.white}>
-            {description}
-          </AppText>
-        </FlexView>
-      </LinearGradient>
-    </ImageBackground>
+        </LinearGradient>
+      </ImageBackground>
+    </Pressable>
   );
 }
 
-export default PlaceCarouselItem;
+export default React.memo(PlaceCarouselItem);
--- src/components/Home/template/PlaceCarousel/index.tsx
+++ src/components/Home/template/PlaceCarousel/index.tsx
@@ -1,6 +1,6 @@
 import PlaceCarouselItem from 'components/Home/module/PlaceCarouselItem';
-import React, {useRef, useState} from 'react';
-import {useWindowDimensions} from 'react-native';
+import React, {useCallback, useRef, useState} from 'react';
+import {ListRenderItem, useWindowDimensions} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import PlaceIndicator from '../PlaceIndicator';
 import {Place} from 'apis/types';
@@ -8,12 +8,15 @@ import FlexView from 'components/@base/FlexView';
 import useRecommandedPlaces from 'hooks/useRecommandedPlaces';
 import PlaceCarouselSkeleton from 'components/Home/module/PlaceCarouselSkeleton';
 import PlaceIndicatorSkeleton from 'components/Home/module/PlaceIndicatorSkeleton';
+import {useNavigation} from '@react-navigation/native';
+import {RootStackNavigationProp} from 'navigations/RootStack/types';
 
 function PlaceCarousel() {
   const {width} = useWindowDimensions();
   const {places, isLoading, parentCategories} = useRecommandedPlaces();
   const [currentPlace, setCurrentPlace] = useState<Place>(places[0]);
   const carouselRef = useRef<Carousel<Place>>(null);
+  const {navigate} = useNavigation<RootStackNavigationProp>();
 
   const handlePressIndicator = (parentId: number) => {
     if (!carouselRef.current) return;
@@ -23,6 +26,24 @@ function PlaceCarousel() {
     );
     carouselRef.current.snapToItem(targetIdx);
   };
+
+  const handlePressCarouselItem = useCallback(
+    (id: number) => {
+      if (currentPlace.id !== id) return;
+      navigate('PlaceDetail', {id});
+    },
+    [currentPlace],
+  );
+
+  const renderItem: ListRenderItem<Place> = useCallback(
+    ({item}) =>
+      isLoading ? (
+        <PlaceCarouselSkeleton />
+      ) : (
+        <PlaceCarouselItem place={item} onPress={handlePressCarouselItem} />
+      ),
+    [isLoading, currentPlace, places],
+  );
 
   return (
     <FlexView className="flex-1 py-4">
@@ -38,17 +59,13 @@ function PlaceCarousel() {
       <Carousel
         ref={carouselRef}
         data={places}
-        renderItem={({item}) =>
-          isLoading ? (
-            <PlaceCarouselSkeleton />
-          ) : (
-            <PlaceCarouselItem place={item} />
-          )
-        }
+        renderItem={renderItem}
+        initialScrollIndex={0}
         itemWidth={width * 0.7}
-        onSnapToItem={idx => setCurrentPlace(places[idx])}
         sliderWidth={width}
+        onSnapToItem={idx => setCurrentPlace(places[idx])}
         onLayout={() => setCurrentPlace(places[0])}
+        containerCustomStyle={{flex: 1}}
         loop
       />
     </FlexView>
