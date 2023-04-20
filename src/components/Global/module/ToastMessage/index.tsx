3ca2835a1b74e9d32082da146fd25341fdfbb335
LTH <johtta888@naver.com>
2023-04-20 22:14:47 +0900

    Design: 추가 ToastMessage onPress guide text

--- src/components/Global/module/ToastMessage/index.tsx
+++ src/components/Global/module/ToastMessage/index.tsx
@@ -17,16 +17,23 @@ function ToastMessage({type, message, onPress}: ToastMessageProps) {
   const {name, color} = iconMap[type];
 
   return (
-    <Pressable onPress={onPress} className="w-full">
-      <FlexView
-        direction="row"
-        gapSize="small"
-        className="items-center p-4 mx-4 bg-black rounded-2xl">
-        <SvgIcon name={name} color={color} />
-        <AppText typoStyle="B2" color={colors.white}>
-          {message}
-        </AppText>
-      </FlexView>
+    <Pressable onPress={onPress} className="w-full ">
+      <View className="flex-row items-center justify-between p-4 mx-4 bg-black rounded-2xl">
+        <FlexView direction="row" gapSize="small" className="items-center">
+          <SvgIcon name={name} color={color} />
+          <AppText typoStyle="B2" color={colors.white}>
+            {message}
+          </AppText>
+        </FlexView>
+        {onPress && (
+          <FlexView direction="row" gapSize="tiny">
+            <AppText typoStyle="B2" color={colors.gray[400]}>
+              보러가기
+            </AppText>
+            <SvgIcon name="chevronRight" size={14} color={colors.gray[400]} />
+          </FlexView>
+        )}
+      </View>
     </Pressable>
   );
 }
