0cd3b624e08e96ac1448564852aeed7161e1f182
LTH <johtta888@naver.com>
2023-04-26 04:10:17 +0900

    Design: 추가 Btn isFit, theme secondary

--- src/components/@base/Btn/index.tsx
+++ src/components/@base/Btn/index.tsx
@@ -3,17 +3,26 @@ import React from 'react';
 import {Pressable, PressableProps, Text} from 'react-native';
 
 type BtnProps = PressableProps & {
-  theme?: 'primary';
+  theme?: 'primary' | 'secondary';
   label: string;
+  isFit?: boolean;
 };
 
-function Btn({theme = 'primary', disabled, label, ...rest}: BtnProps) {
+function Btn({
+  theme = 'primary',
+  disabled,
+  label,
+  isFit = true,
+  ...rest
+}: BtnProps) {
   const {bgColor, textColor} = themeMap[theme];
 
   return (
     <Pressable
       disabled={disabled}
-      className="items-center justify-center p-2 rounded-2xl"
+      className={`${
+        isFit ? 'px-4' : 'flex-1'
+      } items-center justify-center py-2 rounded-2xl`}
       style={{backgroundColor: bgColor, opacity: disabled ? 0.2 : 1}}
       {...rest}>
       <Text
@@ -35,5 +44,9 @@ const themeMap = {
   primary: {
     bgColor: colors.black,
     textColor: colors.white,
+  },
+  secondary: {
+    bgColor: colors.gray[200],
+    textColor: colors.black,
   },
 };
