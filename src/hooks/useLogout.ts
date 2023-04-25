d10e325c304acf766d34a1f5629a5c0b640b0ffe
LTH <johtta888@naver.com>
2023-04-26 04:46:43 +0900

    Fix: 수정 로그아웃 시, 재로그인 불가 오류

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,14 +1,17 @@
+import {useQueryClient} from '@tanstack/react-query';
 import {clearTokenClient} from 'apis/@client';
 import tokenStorage from 'storages/tokenStorage';
 import useUserStore from 'stores/useUserStore';
 
 function useLogout() {
+  const queryClient = useQueryClient();
   const {clear} = useUserStore();
 
   return async () => {
     try {
       clear();
       clearTokenClient();
+      queryClient.invalidateQueries(['myProfile']);
       await tokenStorage.clear();
     } catch (err) {}
   };
