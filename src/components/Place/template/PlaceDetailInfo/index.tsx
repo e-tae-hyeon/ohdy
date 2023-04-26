9d3a9607e27dd8d730f77bd902161ffee79805c5
LTH <johtta888@naver.com>
2023-04-26 18:47:46 +0900

    Fix: 수정 PlaceDetailInfo 줄바꿈 오류

--- src/components/Place/template/PlaceDetailInfo/index.tsx
+++ src/components/Place/template/PlaceDetailInfo/index.tsx
@@ -30,12 +30,12 @@ function PlaceDetailInfo() {
 
   return (
     <FlexView className="py-4">
-      <FlexView direction="row" className="items-baseline px-4">
-        <AppText typoStyle="H2">{name}</AppText>
-        <AppText color={colors.gray[400]}>{category.name}</AppText>
-      </FlexView>
-      <FlexView gapSize="small" className="px-4">
-        <AppText color={'#818CF8'}>{location.name}</AppText>
+      <FlexView className="px-4">
+        <AppText typoStyle="H3">{name}</AppText>
+        <FlexView direction="row" gapSize="small">
+          <AppText color={'#818CF8'}>{location.name}</AppText>
+          <AppText color={colors.gray[400]}>{category.name}</AppText>
+        </FlexView>
         <AppText typoStyle="B2" color={colors.gray[400]}>
           {description}
         </AppText>
@@ -43,7 +43,12 @@ function PlaceDetailInfo() {
       <PlaceDetailTagList tags={keywords.map(keyword => keyword.name)} />
       <FlexView className="px-4">
         <Tag label={`대표메뉴 ${price.toLocaleString()}원`} bgColor="#F1C5C5" />
-        <Tag label={`최대 인원 ${maxHeadCount}명`} bgColor="#FFF9D9" />
+        <Tag
+          label={
+            maxHeadCount < 50 ? `최대 인원 ${maxHeadCount}명` : `인원 제한 없음`
+          }
+          bgColor="#FFF9D9"
+        />
       </FlexView>
       <PlaceDetailTagList
         tags={relationships.map(relationship => relationship.name)}
