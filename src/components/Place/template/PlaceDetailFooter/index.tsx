f851b69c6671c3140725e0e2242fba31f29841d6
LTH <johtta888@naver.com>
2023-04-26 19:54:39 +0900

    Design: 추가 PlaceDetailFooter pb for safe area inset

--- src/components/Place/template/PlaceDetailFooter/index.tsx
+++ src/components/Place/template/PlaceDetailFooter/index.tsx
@@ -7,12 +7,14 @@ import {MainTabNavigationProp} from 'navigations/MainTab/types';
 import {PlaceDetailScreenRouteProp} from 'navigations/RootStack/types';
 import React from 'react';
 import {View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {usePlaceOverrideById} from 'stores/usePlaceOverrideStore';
 
 function PlaceDetailFooter() {
   const {
     params: {id},
   } = useRoute<PlaceDetailScreenRouteProp>();
+  const {bottom} = useSafeAreaInsets();
   const {place} = usePlace(id);
   const placeOverride = usePlaceOverrideById(id);
   const {like, unlike} = useLikePlaceManager();
@@ -41,7 +43,9 @@ function PlaceDetailFooter() {
   };
 
   return (
-    <View className="px-8 py-4 border-t border-neutral-200">
+    <View
+      className="px-8 py-4 border-t border-neutral-200"
+      style={{paddingBottom: bottom + 16}}>
       <PlaceDetailLikeBtn onPress={handlePressLike} isLiked={isLiked} />
     </View>
   );
