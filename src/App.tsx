b5b8e14bfedcf49e4ec86d4f67b94feebbacf297
LTH <johtta888@naver.com>
2023-04-30 02:43:18 +0900

    Feat: 추가 Splash 및 Icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import PolicyBottomSheet from 'components/Auth/template/PolicyBottomSheet';
 import GlobalDialog from 'components/Global/template/GlobalDialog';
 import ToastContainer from 'components/Global/template/ToastContainer';
-import useLoadUser from 'hooks/useLoadUser';
 import RootStack from 'navigations/RootStack';
 import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,6 +17,12 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
+
   return (
     <>
       <SafeAreaProvider>
